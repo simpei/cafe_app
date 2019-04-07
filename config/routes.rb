f37e87b05b30fe8f32a91d9a89d3f39366497d5b
shimblog <info.shimblog@gmail.com>
2019-04-07 21:58:51 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'  
+  devise_for :users
+  root to: 'static_pages#home'  
   get '/about', to: 'static_pages#about'
   get '/serch',  to:'static_pages#serch'
   get '/contact', to:'static_pages#contact'
